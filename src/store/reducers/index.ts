5889de7c470fa401d8a1329bd6b06180d99afe9a
mikejia1 <mike.jia1@gmail.com>
2023-04-28 20:15:58 +1000

    Move all state initialization functions to GlobalState class

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,9 +1,9 @@
 // Reducers take in the current state and an action and return a new state.
 // They are responsible for processing all game logic.
 
-import { Direction, computeCurrentFrame, worldBoundaryColliders, tileRect, rectanglesOverlap, randomInt, ALL_DIRECTIONS, Coord, GardenerDirection } from "../../utils";
-import { Collider, ColliderType, ColliderExceptions, IGlobalState } from "../classes";
-import { Gardener, NonPlayer, WateringCan, Plant, INITIAL_PLANT_HEALTH } from '../../entities';
+import { Direction, computeCurrentFrame, rectanglesOverlap, randomInt, ALL_DIRECTIONS, GardenerDirection } from "../../utils";
+import { Collider, ColliderType, ColliderExceptions, IGlobalState, initialGameState } from "../classes";
+import { NonPlayer, Plant } from '../../entities';
 import {
   DOWN,
   INCREMENT_SCORE,
@@ -28,147 +28,6 @@ import {
   TOGGLE_DEBUG_CONTROL_DISABLE_COLLISIONS,
   TOGGLE_GAME_AUDIO,
 } from "../actions";
-import { V_TILE_COUNT, H_TILE_COUNT, collisions, plants, MAP_TILE_SIZE } from "../data/positions";
-import { InvisibleCollider } from "../../scene";
-
-// Gardener images.
-import basewalkstrip     from "../../entities/images/gardener/base_walk_strip8.png";
-import basewateringstrip from "../../entities/images/gardener/base_watering_strip5.png";
-import toolwateringstrip from "../../entities/images/gardener/tools_watering_strip5.png";
-
-// Blast shield images.
-import closedShield from "../../entities/images/shield/shield_32x160.png";
-import topShield    from "../../entities/images/shield/shield_top_32x.png";
-import bottomShield from "../../entities/images/shield/shield_bottom_32x.png";
-
-// Other images.
-import npcwalkcycle from "../../entities/images/nonplayer/npcwalkcycle.png";
-import spacegarden  from "../images/space_garden.png";
-import wateringcan  from "../../entities/images/wateringcan/wateringcan.png";
-import spaceframes  from "../images/space_frames.png";
-
-// Plant image.
-import plantimage from "../../entities/images/plant/plants_16x16.png";
-
-// Default gardener starting state.
-function initialGardener(colliderId: number): Gardener {
-  return new Gardener(colliderId, new Coord(200, 220), GardenerDirection.Right, false, false, false);
-}
-
-// Create watering can for start of game.
-function initialWateringCan(): WateringCan {
-  return new WateringCan(new Coord(200, 150), false);
-}
-
-// Generate the game starting state.
-function initialGameState(): IGlobalState {
-  // Ensure all colliders get a unique ID.
-  let colliderId = 0;
-
-  // Create invisible colliders for map features with unique collider Ids and increment colliderId accordingly.
-  let features = invisibleCollidersForMapFeatures(colliderId);
-  colliderId += features.length;
-
-  // Create plants from initial plant positions. 
-  let allPlants = createPlants(colliderId);
-  colliderId += allPlants.length;
-
-  // Create invisible colliders for world boundaries with unique collider Ids and increment colliderId accordingly.
-  let worldBoundaries = worldBoundaryColliders(colliderId);
-  colliderId += worldBoundaries.length;
-
-  // Create a bunch of NPCs and increment colliderId accordingly.
-  let npcs = gridOfNPCs(colliderId, new Coord(200, 250), 25, 2, 2);
-  colliderId += npcs.length;
-
-  return {
-    gardener: initialGardener(colliderId++),
-    keysPressed: [],
-    score: 0,
-    wateringCan: initialWateringCan(),
-    plants: allPlants,
-    npcs: npcs,
-    currentFrame: 0,
-    gardenerImages: {
-      walkingBase:  loadImage("Base walk strip", basewalkstrip),
-      wateringBase: loadImage("Base watering strip", basewateringstrip),
-      waterPouring: loadImage("Tool watering strip", toolwateringstrip),
-    },
-    npcimage:         loadImage("NPC walk cycle", npcwalkcycle),
-    backgroundImage:  loadImage("Space garden", spacegarden),
-    wateringCanImage: loadImage("Watering can", wateringcan),
-    deepSpaceImage:   loadImage("Space frames", spaceframes),
-    shieldImages: {
-      closed:         loadImage("Closed shield", closedShield),
-      top:            loadImage("Top shield", topShield),
-      bottom:         loadImage("Bottom shield", bottomShield),
-    },
-    plantImage:       loadImage("Plant image", plantimage),
-    invisibleColliders: [...worldBoundaries, ...features],  // Map features and world boundaries both contribute invisible colliders.
-    muted: true,
-    debugSettings: {
-      showCollisionRects: false,   // Collision rectangles for colliders.
-      showPositionRects: false,    // Position rectangles for paintables.
-      showWateringRects: false,    // Watering interaction rectangles for plants.
-      showFacingRects: false,      // Facing direction rectangle for gardener.
-      showEquipRects: false,       // Equipping interaction rectangle for watering can.
-      collisionsDisabled: false,   // Disable collision checks - Gardener can walk through anything.
-    },
-  }
-}
-
-function loadImage(title: string, resource: string) {
-  const img = new Image();
-  img.src = resource;
-  img.onload = () => {
-      console.log("Image loaded: " + title);
-  };
-  return img;
-}
-
-// Create a grid of NPCs with top-left one at given position, and with given spacing.
-function gridOfNPCs(colliderId: number, pos: Coord, spacing: number, cols: number, rows: number): NonPlayer[] {
-  let all: NonPlayer[] = [];
-  for (let col = 0; col < cols; col++) {
-    for (let row = 0; row < rows; row++) {
-      let npc = new NonPlayer({
-        colliderId: colliderId + (row * cols) + col,
-        pos: pos.plus(col * spacing, row * spacing), 
-      });
-      all = [...all, npc];
-    }
-  }
-  return all;
-}
-
-function createPlants(colliderId: number): Plant[]{
-  let all: Plant[] = [];
-  for (let r = 0; r < V_TILE_COUNT; r++) {
-    for (let c = 0; c < H_TILE_COUNT; c++) {
-      let i = (r * H_TILE_COUNT) + c;
-      if (plants[i] == 0) continue;
-      // Shift the vertical position of plants by 4 pixels to better align with dirt patch. 
-      let plant: Plant = new Plant(colliderId++, new Coord(c*MAP_TILE_SIZE, r*MAP_TILE_SIZE+8), INITIAL_PLANT_HEALTH);
-      all = [...all, plant];
-    }
-  }
-  return all;
-}
-
-// Return an array of invisible colliders based on store/data/collision.tsx
-function invisibleCollidersForMapFeatures(nextColliderId: number): Collider[] {
-  let all: Collider[] = [];
-  for (let r = 0; r < V_TILE_COUNT; r++) {
-    for (let c = 0; c < H_TILE_COUNT; c++) {
-      let i = (r * H_TILE_COUNT) + c;
-      if (collisions[i] == 0) continue;
-      let ic = new InvisibleCollider(nextColliderId + all.length, tileRect(r,c));
-      all = [...all, ic];
-    }
-  }
-  return all;
-}
-
 // All actions/index.ts setters are handled here
 const gameReducer = (state = initialGameState(), action: any) => {
   switch (action.type) {
