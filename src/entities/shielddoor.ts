b8f89b47e266be4a0d4a4b8df5c1a228e58ad719
mikejia1 <mike.jia1@gmail.com>
2023-05-05 12:05:32 +1000

    Add shieldDoor skeleton class

---
+++ src/entities/shielddoor.ts
@@ -0,0 +1,21 @@
+import { IGlobalState } from "../store/classes";
+import { Paintable } from "../store/classes/paintable";
+import { Coord } from "../utils";
+
+enum ShieldDoorState {OPENING, CLOSING, OPEN, CLOSED}
+
+export class ShieldDoor implements Paintable {
+    // Unused coords
+    pos: Coord;
+    // There are 3 shield doors
+    sheildDoorStates: ShieldDoorState[];
+    
+    constructor() {
+        this.pos = new Coord(0,0);
+        this.sheildDoorStates = [ShieldDoorState.CLOSED, ShieldDoorState.CLOSED, ShieldDoorState.CLOSED];
+    }
+
+    paint(canvas: CanvasRenderingContext2D, state: IGlobalState): void {
+        //paint doors
+    }
+}
