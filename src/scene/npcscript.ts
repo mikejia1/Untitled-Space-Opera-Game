1b7a22a30696eef146b829012968612b596b2b41
kjleegraham <129822779+kjleegraham@users.noreply.github.com>
2023-06-09 00:18:39 +1000

    A few more dialog strings

--- src/scene/npcscript.ts
+++ src/scene/npcscript.ts
@@ -5,6 +5,8 @@ export const cabinFeverDialog : string[] = [
     "Darkness envelopes our ship like a\n protean womb.",
     "I think I'm seeing things...",
     "That planet looks bad for my mental\n health.",
+    "Is it crazy in here or is it just me?",
+    "I feel funny. Like, BAD funny!",
 ] 
 
 export const movingToAirlockDialog : string[] = [
@@ -12,6 +14,7 @@ export const movingToAirlockDialog : string[] = [
     "My insides are on fire!",
     "My eyeballs! They itch! THEY ITCH!",
     "Have you ever strangled a kitten?",
+    "Airlock shmairlock! I'm not afraid!",
 ] 
 
 export const contemplatingDeathDialog : string[] = [
@@ -19,6 +22,7 @@ export const contemplatingDeathDialog : string[] = [
     "It's too stuffy in here, I need some air!",
     "I think I'l go for a walk outside...",
     "Ph'nglui mglw'nafh Cthulhu R'lyeh\n wgah'nagl fhtagn.",
+    "The vacuum of space is where we'll\n find peace!",
 ]
 
 export const catInvasionDialog : string[] = [
@@ -27,6 +31,7 @@ export const catInvasionDialog : string[] = [
     "Don't pet the kitty!",
     "Is that meowing I hear?",
     "Cats are evil!",
+    "Space cats are so cute I could DIE!",
 ]
 
 export const scorchingStarDialog: string[] = [
@@ -35,11 +40,14 @@ export const scorchingStarDialog: string[] = [
     "Those plants don't look so good...",
     "Who left the sauna open?",
     "Are those plants still alive?",
+    "Damn it! I forgot to pack sunblock!",
 ]
 
 export const blackHoleDialog: string[] = [
     "That can't be good.",
     "We're dooooooomed!",
+    "We're passing way too close to that\n black hole.",
+    "Black holes are harmless right?\n Right?!...",
 ]
 
 export const tutorialDialog: string[] = [
